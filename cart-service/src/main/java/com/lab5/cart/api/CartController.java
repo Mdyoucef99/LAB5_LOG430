719b31ed19c525a6fbfe068dfe583f0e95dffa9b
Youcef <mekki.d.youcef@outlook.fr>
2025-07-14 15:26:42 -0400

    fix cart service

--- cart-service/src/main/java/com/lab5/cart/api/CartController.java
+++ cart-service/src/main/java/com/lab5/cart/api/CartController.java
@@ -5,45 +5,73 @@ import com.lab5.cart.infrastructure.CartItemDao;
 import com.lab5.cart.domain.Cart;
 import com.lab5.cart.domain.CartItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.sql.SQLException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/carts")
+@RequestMapping("/api/v1/carts")
 public class CartController {
+    private final CartDao cartDao;
+    private final CartItemDao cartItemDao;
+
     @Autowired
-    private CartDao cartDao;
-    @Autowired
-    private CartItemDao cartItemDao;
+    public CartController(CartDao cartDao, CartItemDao cartItemDao) {
+        this.cartDao = cartDao;
+        this.cartItemDao = cartItemDao;
+    }
 
     @GetMapping
-    public List<Cart> getAllCarts() throws SQLException {
-        return cartDao.findAll();
+    public ResponseEntity<List<Cart>> getAllCarts() {
+        try {
+            return ResponseEntity.ok(cartDao.findAll());
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @PostMapping
-    public Cart createCart(@RequestBody Cart cart) throws SQLException {
-        cartDao.create(cart);
-        return cart;
+    public ResponseEntity<Cart> createCart(@RequestBody Cart cart) {
+        try {
+            cartDao.create(cart);
+            return ResponseEntity.status(HttpStatus.CREATED).body(cart);
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping("/{id}")
-    public Cart getCart(@PathVariable int id) throws SQLException {
-        return cartDao.findById(id);
+    public ResponseEntity<Cart> getCart(@PathVariable int id) {
+        try {
+            Cart cart = cartDao.findById(id);
+            if (cart == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            return ResponseEntity.ok(cart);
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @PostMapping("/{cartId}/items")
-    public CartItem addItemToCart(@PathVariable int cartId, @RequestBody CartItem item) throws SQLException {
-        // On force le cartId reçu dans l'URL
-        CartItem newItem = new CartItem(cartId, item.getProductId(), item.getQuantity());
-        cartItemDao.create(newItem);
-        return newItem;
+    public ResponseEntity<CartItem> addItemToCart(@PathVariable int cartId, @RequestBody CartItem item) {
+        try {
+            CartItem newItem = new CartItem(cartId, item.getProductId(), item.getQuantity());
+            cartItemDao.create(newItem);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newItem);
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping("/{cartId}/items")
-    public List<CartItem> getCartItems(@PathVariable int cartId) throws SQLException {
-        return cartItemDao.findByCartId(cartId);
+    public ResponseEntity<List<CartItem>> getCartItems(@PathVariable int cartId) {
+        try {
+            return ResponseEntity.ok(cartItemDao.findByCartId(cartId));
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 } 
